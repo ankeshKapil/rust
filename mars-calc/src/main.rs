ca9c99619b897a45d2c2bd8d25e317d3c1fecc08
Ankesh Kapil <ankesh.kapil@gmail.com>
2023-08-15 19:04:41 +0530

    function

--- mars-calc/src/main.rs
+++ mars-calc/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mars_weight = calculates_weight_on_mars(95.00);
+    println!("Our Weight on mars {} Kg",mars_weight);
+}
+
+fn calculates_weight_on_mars(weight: f32) -> f32{
+    (weight/ 9.81) * 3.711
 }
